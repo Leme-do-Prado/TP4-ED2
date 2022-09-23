4a11c6ea42dd723e063cdeb456c139fb64e52332
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-09-23 18:02:38 -0300

    Update Data.cs

--- Data.cs
+++ Data.cs
@@ -3,13 +3,42 @@ public class Data{
     private int mes;
     private int ano;
 
-    public void setData(int d, int  m, int a){
+    public Data(int d, int  m, int a){
         this.dia = d;
         this.mes = m;
         this.ano = a;
     }
 
-    public override ToString(){
+    public Data(){
+        int a, b, c = Console.ReadInt();
+        Data(a, b, c);
+    }
+
+    public void setDia(int d){
+        this.dia = d;
+    }
+
+    public void setMes(int m){
+        this.mes = d;
+    }
+
+    public void setAno(int a){
+        this.ano = a;
+    }
+
+    public int getDia(){
+        return this.dia;
+    }
+
+    public int getMes(){
+        return this.mes;
+    }
+
+    public int getAno(){
+        return this.ano;
+    }
+
+    public override String ToString(){
         String stringdata = dia + "/" + mes + "/" ano;
         return stringdata;
     }
