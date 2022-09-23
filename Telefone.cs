78443b63b47cd00483559c4f3c882da557cad3f0
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-09-23 18:03:18 -0300

    Update Telefone.cs

--- Telefone.cs
+++ Telefone.cs
@@ -2,4 +2,39 @@ public class Telefone{
     private String tipo;
     private String numero;
     private bool principal;
+
+    public Telefone(String t, String n, bool p){
+        this.tipo = t;
+        this.numero = n;
+        this.principal = p;
+    }
+
+    public Telefone(){
+        int a, b, c = Console.ReadInt();
+        Telefone(a, b, c);
+    }
+
+    public void setTipo(String t){
+        this.tipo = t;
+    }
+
+    public void setNumero(String n){
+        this.numero = n;
+    }
+
+    public void setPrincipal(bool p){
+        this.principal = p;
+    }
+
+    public String getTipo(){
+        return this.tipo;
+    }
+
+    public String getNumero(){
+        return this.numero;
+    }
+
+    public bool getPrincipal(){
+        return this.principal;
+    }
 }
